70fc9bfff815e1d3073410e066d458c0ce3705a9
jamauro <jamauro@gmail.com>
2023-10-31 13:14:19 -0500

    update version

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'jam:method',
-  version: '0.1.0',
+  version: '1.0.0-beta',
   summary: 'An easy way to create Meteor methods',
   git: 'https://github.com/jamauro/method',
   documentation: 'README.md'
