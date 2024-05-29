b193d0fe8e7a037f137eff7764b7bcc910d68991
Dominique Quatravaux <dominique.quatravaux@epfl.ch>
2024-05-29 07:44:21 +0200

    [feature] Meteor 3.0 compatibility

--- package.js
+++ package.js
@@ -1,16 +1,16 @@
 Package.describe({
   name: 'jam:method',
-  version: '1.5.1',
+  version: '2.0.0-rc1',
   summary: 'An easy way to create Meteor methods',
   git: 'https://github.com/jamauro/method',
   documentation: 'README.md'
 });
 
 Package.onUse(function(api) {
-  api.versionsFrom('2.8.1');
+  api.versionsFrom('2.8.1', '3.0-alpha.19');
   api.use('ecmascript');
   api.use('check');
-  api.use('mongo');
+  api.use('mongo@2.0.0-rc300.2');
   api.use('ddp-rate-limiter', 'server');
   api.use('jam:easy-schema@1.3.1-alpha300.19', { weak: true });
   api.use('zodern:types@1.0.11');
